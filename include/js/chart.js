100a680b588e8630ba0d6f73f399cef738f8a495
e.ngbamekoyapolo <e.ngbamekoyapolo@technologyandstrategy.com>
2023-08-03 16:37:55 +0200

    MAJ le tableau de mes compétences

---
+++ include/js/chart.js
@@ -0,0 +1,39 @@
+var ctx = document.getElementById('barchart').getContext('2d');
+var barchart = new Chart(ctx, {
+    type: 'bar', //doughnut
+    data: {
+        labels: ['Flutter', 'C# .Net Core', 'Python', 'Kotlin', 'Arduino', 'Angular'],
+        datasets: [{
+            label: 'Technologie',
+            data: [13, 19, 15, 13, 10, 8],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+                'rgba(255, 99, 132, 0.5)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
