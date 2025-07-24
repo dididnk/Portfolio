dd68ae19323a45a28fae79c39dd3d88cdb23f472
Emmanuel Ngbame <ngbamedistingue@yahoo.com>
2025-07-24 13:43:35 +0200

    UI: toogle button init + removing bootstrap

--- ngbame/src/App.tsx
+++ ngbame/src/App.tsx
@@ -1,17 +1,15 @@
-import { Home } from "lucide-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ ngbame/src/components/ThemeToogle.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleTheme =()=>{
+    if(isDarkMode){
+      setIsDarkMode(false);
+    }else{
+      setIsDarkMode(true);
+    }
+  }
+
+  return (
+    <button onClick={toggleTheme}>
+      {""}
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300"/>
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900"/>
+      )}
+    </button>
+  );
+};
